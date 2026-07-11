bb0338f11ef6b7ebd92a42bfe8e435cd2b9e9f41
alexgul25 <fine.9ulyaev@yandex.ru>
2026-07-11 17:58:12 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Notify Service (notify-svc)
+
+Микросервис для проекта **Date Wishlist Hub**, отвечает за отправку уведомлений пользователям.  
+
+Стек: `Go`  `gRPC`  `Kafka`  `Redis`
---
+++ cmd/svc-starter/main.go
@@ -0,0 +1,25 @@
+package main
+
+func main() {
+
+	/*
+		- написать  internal/domain/ (пакет events и файл с ошибками)
+
+		- написать internal/lib с логгером
+
+		- написать internal/service/notification/logic.go с абстракциями
+
+		- написать internal/storage/redis для ключей
+
+		- написать internal/infrastructure/ (подпапки grpc, kafka, notifier, serializer)
+
+		- написать internal/consumer где определить Consumer и процессор, использующий собранные ранее компоненты
+
+		- написать internal/config
+
+		- написать internal/app
+
+		- собрать всё в main
+	*/
+
+}
